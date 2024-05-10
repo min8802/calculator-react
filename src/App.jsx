fb21b2ed7952cd7c519a5c6ba054a3d1b93c8732
min8802 <ww330@naver.com>
2024-05-10 17:25:05 +0900

    color edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Calculator from "./components/Calculator";
 
 const App = () => {
   return (
-    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+    <div className="bg-white min-h-screen flex justify-center items-center">
       <Calculator />
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({name, onClickButton}) => {
     return (
-    <button className="bg-gray-200 flex justify-center w-10 h-10 items-center rounded-md" onClick={onClickButton}>
+    <button className={`bg-gray-100 flex justify-center w-20 h-10 items-center rounded-md ${name === "+" && "bg-blue-500"}`} onClick={onClickButton}>
         {name}
     </button>
     );
