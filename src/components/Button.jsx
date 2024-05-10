a60ad308a7dcfaa973b14c7a67fd5d370f8d3e7c
min8802 <ww330@naver.com>
2024-05-10 13:39:26 +0900

    UI Setting

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+const Button = ({name}) => {
+    return (
+    <button className="bg-gray-200 flex justify-center w-10 h-10 items-center rounded-md">
+        {name}
+    </button>);
+  };
+  
+  export default Button;
---
+++ src/components/Result.jsx
@@ -0,0 +1,5 @@
+const Result = () => {
+    return <div className="bg-gray-50 p-2 rounded-md shadow-md">result</div>
+};
+
+export default Result
